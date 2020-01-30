ccb500ec43e0a3108ee764d46f13e7c42b09c82d
Ratko Solar <ratko.solar@gmail.com>
2020-01-30 22:00:39 +0100

    Add _redirects file for netlify deploy

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -17,7 +18,14 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css'
-        })
+        }),
+        new CopyPlugin([
+            {
+                from: '_redirects',
+                toType: 'file',
+                to: '_redirects'
+            },
+        ]),
     ],
     module: {
         rules: [
